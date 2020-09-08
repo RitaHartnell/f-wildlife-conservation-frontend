68c0f92eef3c71a7601a361c512c82aeae8fb504
hahm-d <dhahm01@gmail.com>
2020-09-08 00:16:59 -0400

    home page sample

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo2 from '../logo/logo2.png'
 
 const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <h2>sample image </h2>
+            <img src={logo2} alt="logo sample"/>
+            <h3>About us: ...</h3>
         </div>
     );
 };
